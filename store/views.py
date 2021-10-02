3271c96c7f2461768b3d41a17b23e3c32406e555
Abhijeet Singh Gureniya <abhigureniya1@gmail.com>
2021-10-02 18:36:15 +0530

    Product Grid Completed with Templates

--- store/views.py
+++ store/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from .models import Product, Category
 
+app_name="store"
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products':products})
