0387794f6879285782338c01a14d10ec7e5adf6c
Abhijeet Singh Gureniya <abhigureniya1@gmail.com>
2021-10-03 13:32:00 +0530

    Refactored Code: Before Session Implementation

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return super(ProductManager, self).get_queryset().filter(is_active=True)
@@ -26,7 +27,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default="admin")
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to = 'images/')
+    image = models.ImageField(upload_to = 'images/', default='images/default.png')
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -6,7 +6,7 @@ from django.test import Client, RequestFactory, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
-from store.views import all_products
+from store.views import product_all
 
 
 @skip("demonstrating skipping")
@@ -30,7 +30,7 @@ class TestViewResponses(TestCase):
         """
         response = self.c.get('/', HTTP_HOST='noaddress.com')
         self.assertEqual(response.status_code, 400)
-        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_url(self):
@@ -61,9 +61,9 @@ class TestViewResponses(TestCase):
         Example: code validation, search HTML for text
         """
         request = HttpRequest()
-        response = all_products(request)
+        response = product_all(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>', html)
+        self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
 
@@ -72,8 +72,8 @@ class TestViewResponses(TestCase):
         Example: Using request factory
         """
         request = self.factory.get('/item/django-beginners')
-        response = all_products(request)
+        response = product_all(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>', html)
+        self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
