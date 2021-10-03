0b20446b5b1e804b0c56e049856a7da239cd7cd4
Abhijeet Singh Gureniya <abhigureniya1@gmail.com>
2021-10-03 12:35:27 +0530

    Refactored Code

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6s!*j53omdl@3kiz7kwg255zwe&fv*f_6seu42ba7fw1vhg(8=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com','127.0.0.1']
 
 
 # Application definition
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.views.categories',
+                'store.context_processors.categories',
             ],
         },
     },
